1d403e82417c1fd734f09c0198d88e4fdd20ac33
Gentry Rolofson <xdarkicex@gmail.com>
2016-08-01 22:03:36 -0700

    building gem file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def stylesheet_if_exists(name)
+    return ((File.exist?('app/assets/stylesheets/'+name+'.scss'))? (stylesheet_link_tag name, media: 'all', 'data-turbolinks-track' => true) : '')
+  end
+  def javascript_if_exists(name)
+    return ((File.exist?('app/assets/javascripts/'+name+'.js'))? (javascript_include_tag name, 'data-turbolinks-track' => true) : '')
+  end
+  def assets_if_exists(name); stylesheet_if_exists(name) + javascript_if_exists(name); end
 end
